947d32da346931010549bd238750862c87d7f577
shivmatkawala <shivkumarmatkawala@gmail.com>
2026-01-31 23:56:14 +0530

    done done

--- csmitbackend/settings.py
+++ csmitbackend/settings.py
@@ -38,9 +38,7 @@ INSTALLED_APPS = [
     'users', 'resume', 'practice', 'jobs', 'exams', 'doubts',
     'courses', 'batches', 'drf_yasg', 'announcements', 'inquiries',
     'success_stories', 'blogs', 'notes', 'careers','job_applications',
-    'executor',
-    'executor',
-    'success_stories', 'blogs', 'notes', 'careers','job_applications', 'zoom',
+    'executor','zoom',
 ]
 
 MIDDLEWARE = [
